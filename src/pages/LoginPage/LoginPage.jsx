fb53b7923edb998ba1979b2d2e11db92c56552b3
Aakash-Sleur <dsaakash463@gmail.com>
2025-08-07 11:32:06 +0530

    init

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,18 @@
+import LoginForm from '../../components/LoginForm/LoginForm'
+import LoginImageSection from '../../components/LoginImageSection/LoginImageSection'
+import './login-page.scss'
+
+export default function LoginPage() {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="form-section">
+          <LoginForm />
+        </div>
+        <div className="image-section">
+          <LoginImageSection />
+        </div>
+      </div>
+    </div>
+  )
+}
