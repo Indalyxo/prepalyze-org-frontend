7c0cf8ac3ae8627bac1f150fbf51357009a0453b
suresh C <sureshs68167@gmail.com>
2025-08-28 21:14:56 +0530

    features:organisation settings ui complted

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import "./settings.scss"
+import React from "react";
+import { TextInput, NumberInput, Textarea, Button, Paper, Title } from "@mantine/core";
+import "./Settings.scss";
 
 const Settings = () => {
   return (
-    <div>Settings</div>
-  )
-}
+    <div className="settings-container">
+      <Paper shadow="sm" radius="md" p="xl" withBorder>
+        <Title order={2} className="settings-title">
+          Organization Settings
+        </Title>
 
-export default Settings
+        <form className="settings-form">
+         
+
+          {/* Detention Max Tabs */}
+          <NumberInput
+            label="Max Tabs"
+            placeholder="Enter max tabs"
+            className="settings-field"
+          />
+
+          {/* Detention Duration */}
+          <NumberInput
+            label="Detention Duration (minutes)"
+            placeholder="Enter duration"
+            className="settings-field"
+          />
+
+          {/* Exam Instructions */}
+          <Textarea
+            label="Exam Instructions"
+            placeholder="Enter exam instructions"
+            minRows={3}
+            className="settings-field"
+          />
+
+          <Button type="submit" size="lg" className="settings-button">
+            Save Settings
+          </Button>
+
+        </form>
+      </Paper>
+    </div>
+  );
+};
+
+export default Settings;
